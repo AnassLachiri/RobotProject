5dc12d10f38109c51cc035e07316bd7df39b3da5
AnassLachiri <lachirianass@gmail.com>
2020-01-15 21:59:04 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -15,7 +15,7 @@ SDL_Surface* createSurface(SDL_Window * window, SDL_Renderer * rend);
 SDL_Texture * createTexture(SDL_Window * window, SDL_Renderer * rend, SDL_Surface * surface);
 
 
-int handleEvents(SDL_Window * window, int *w, int *h, int *up, int *right, int *down, int *left);
+int handleEvents(SDL_Window * window, int *w, int *h, int *up, int *right, int *down, int *left, SDL_Rect *pos, SDL_Rect shapes[], int rands[], int* shapeNbr, int tab[]);
 
 
 void controlRobot(int up, int down, int right, int left, int speed, int *x_vel, int *y_vel);
@@ -25,5 +25,8 @@ void rotationRobot(int up, int down, int right, int left, int *rot);
 
 void updateRobotPosition(SDL_Rect *pos, int x_vel, int y_vel, int w, int h);
 
+void checkCircle(SDL_Rect *pos, SDL_Rect shapes[], int rands[], int* shapeNbr);
+
+void putCircle(SDL_Rect *pos, SDL_Rect shapes[],int rands[], int* shapeNbr, int tab[]);
 
 #endif
